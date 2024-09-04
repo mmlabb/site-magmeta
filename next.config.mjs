91116b36a83c5718f6adaaee31651e774e7d9d6b
mmlab <162450963+mmlabb@users.noreply.github.com>
2024-09-03 23:20:28 -0300

    deploy test

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/mmtest",
   output: "export",
   reactStrictMode: true,
   assetPrefix: "https://mmlabb.github.io/mmtest/",
