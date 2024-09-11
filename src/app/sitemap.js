c3e7f2aa99ec65948a9b6eb782830ae2d77ea3a3
mmlab <162450963+mmlabb@users.noreply.github.com>
2024-09-11 17:11:29 -0300

    feat: SEO robots and sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,18 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap() {
+  return [
+    {
+      url: "https://magmeta.com.br",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 1,
+    },
+    {
+      url: "https://magmeta.com.br/a-magmeta",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+  ];
+}
