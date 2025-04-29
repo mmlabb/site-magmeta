aee38712a851aa90c5148f4fafd9d001f249cdb1
Vanz-silva <162450963+mmlabb@users.noreply.github.com>
2025-04-29 17:16:57 -0300

    Ajuste de tamanho das colunas do grid blog

--- src/components/ArticleGrid.js
+++ src/components/ArticleGrid.js
@@ -26,7 +26,7 @@ const ArticleGrid = ({ articles, limit, selectedTag }) => {
         {lista.length > 0 ? (
           lista.map((article) => (
             <div
-              className="col-md-4"
+              className="col-md-6"
               key={article.slug}
               onClick={() => goArticle(article.slug)}
               style={{ cursor: "pointer" }}
@@ -84,7 +84,7 @@ const ArticleGrid = ({ articles, limit, selectedTag }) => {
           <p className="text-center">Nenhum artigo encontrado para essa tag.</p>
         )}
       </div>
-      {!!limit && filteredArticles.length >= limit ? (
+      {!!limit ? (
         <div className="row pt-5">
           <div className="d-flex mt-4 justify-content-center">
             <Link
