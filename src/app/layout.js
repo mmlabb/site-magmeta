b22cc0e7b8b397854a9b5db7dadf1144fba4b8e9
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2025-06-09 20:55:13 -0300

    fix: Ajuste seo

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
   title: "Magmeta Soluções",
   description:
     "Tudo o que seu negócio precisa para dominar o digital! Ferramentas e estratégias otimizadas especialmente para o seu crescimento.",
-  applicationName: "Magmeta digital",
+  applicationName: "Magmeta Digital",
   referrer: "origin-when-cross-origin",
   keywords: [
     "Magmeta",
@@ -72,7 +72,30 @@ export const metadata = {
     "Blog",
     "SaaS",
   ],
-  thumbnail: "https://magmeta.com.br/assets/img/ad-thumb.jpg",
+  authors: [{ name: "Magmeta", url: "https://magmeta.com.br" }],
+  creator: "Magmeta",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/favicon.ico",
+  },
+  metadataBase: new URL("https://magmeta.com.br"),
+  openGraph: {
+    title: "Magmeta Soluções",
+    description:
+      "Tudo o que seu negócio precisa para dominar o digital! Ferramentas e estratégias otimizadas especialmente para o seu crescimento.",
+    url: "https://magmeta.com.br",
+    siteName: "Magmeta",
+    images: [
+      {
+        url: "/assets/img/ad-thumb.jpg",
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: "pt_BR",
+    type: "website",
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -80,32 +103,32 @@ export default function RootLayout({ children }) {
     <html lang="pt-BR">
       <head>
         <MetaPixel />
-        <body>
-          <ThemeProvider>
-            <div id="app-theme" className="theme-dark">
-              <Navbar />
-              <a
-                href="https://api.whatsapp.com/send?phone=+5561981689203&text=Ol%C3%A1%2C%20vim%20pelo%20site%20e%20quero%20saber%20mais%20sobre%20as%20Solu%C3%A7%C3%B5es%20Magmeta."
-                target="_blank"
-                className="botao-whatsapp"
-              >
-                <Image
-                  src="/assets/img/logo-wpp.svg"
-                  alt="IconeWpp"
-                  width={0}
-                  height={0}
-                  sizes="100%"
-                  style={{ width: "auto", height: "auto" }} 
-                />
-              </a>
-              <main>{children}</main> 
-              <Footer /> 
-              <PopupBlog />
-            </div>
-          </ThemeProvider>
-          <GoogleAnalytics gaId="G-T8V8KCT7DP" />
-        </body>
       </head>
+      <body>
+        <ThemeProvider>
+          <div id="app-theme" className="theme-dark">
+            <Navbar />
+            <a
+              href="https://api.whatsapp.com/send?phone=+5561981689203&text=Ol%C3%A1%2C%20vim%20pelo%20site%20e%20quero%20saber%20mais%20sobre%20as%20Solu%C3%A7%C3%B5es%20Magmeta."
+              target="_blank"
+              className="botao-whatsapp"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/assets/img/logo-wpp.svg"
+                alt="Ícone WhatsApp"
+                width={40}
+                height={40}
+                style={{ width: "auto", height: "auto" }}
+              />
+            </a>
+            <main>{children}</main>
+            <Footer />
+            <PopupBlog />
+          </div>
+        </ThemeProvider>
+        <GoogleAnalytics gaId="G-T8V8KCT7DP" />
+      </body>
     </html>
   );
 }
