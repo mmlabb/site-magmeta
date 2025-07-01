e5180a6799480f42f8ed99595cc6ddd92bf6f698
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2025-07-01 17:57:21 -0300

    feat: Implementando accordion no componente Faq

--- src/components/pacote/FaqSection.js
+++ src/components/pacote/FaqSection.js
@@ -1,30 +1,73 @@
 "use client";
 import Lottie from "lottie-react";
+import { useState } from "react";
 
-export default function FaqSection({ titulo, animationData, perguntas }) {
+export default function FaqSection({
+  titulo,
+  animationData,
+  perguntas,
+  backgroundColor,
+}) {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const toggleIndex = (index) => {
+    setActiveIndex(activeIndex === index ? null : index);
+  };
+
   return (
-    <section className="mt-5 pt-5">
-      <div className="container  faq-box">
-        <div className="row mb-4 p-5">
+    <section
+      className="my-5 pt-5"
+      style={{ backgroundColor, borderRadius: "24px" }}
+    >
+      <div className="container faq-box">
+        <div className="row  p-5">
           <div className="col-12 col-md-6">
-            <span className="py-2">FAQ</span>
-            <h3>{titulo}</h3>
-            <div>
+            <div className="px-3 pt-4">
+              <span className="pb-4">FAQ</span>
+              <h3>{titulo}</h3>
+            </div>
+            <div
+              className="accordion d-flex pt-4 flex-column "
+              id="faqAccordion"
+            >
+              {perguntas.map(({ pergunta, resposta }, index) => (
+                <div
+                  className="accordion-item"
+                  key={index}
+                  style={{ backgroundColor: "transparent" }}
+                >
+                  <h2 className="accordion-header">
+                    <button
+                      className={`accordion-button ${
+                        activeIndex === index ? "" : "collapsed"
+                      }`}
+                      type="button"
+                      onClick={() => toggleIndex(index)}
+                    >
+                      <h4 className="mb-0">{pergunta}</h4>
+                    </button>
+                  </h2>
+                  <div
+                    className={`accordion-collapse collapse ${
+                      activeIndex === index ? "show" : ""
+                    }`}
+                  >
+                    <div className="accordion-body">{resposta}</div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className="col-12 col-md-6 col-lot">
+            <div className="d-flex justify-content-end">
               <Lottie
                 animationData={animationData}
                 loop={true}
-                className="w-100"
-                style={{ maxWidth: "74%", height: "100%" }}
+                className="w-100 lott"
+                style={{ maxWidth: "68%", height: "100%" }}
               />
             </div>
-          </div>
-          <div className="col-12 col-md-6 pt-4">
-            {perguntas.map(({ pergunta, resposta }, index) => (
-              <div key={index} className="border-bottom py-2">
-                <h6>{pergunta}</h6>
-                <p>{resposta}</p>
-              </div>
-            ))}
           </div>
         </div>
       </div>
