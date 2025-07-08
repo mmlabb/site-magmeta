c85ed9476bffe09c79ec110a4f9e170436df9db7
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2025-07-08 11:51:50 -0300

    feat: Separando imgs do banner principal soluções

--- src/components/pacote/BannerPrincipal.js
+++ src/components/pacote/BannerPrincipal.js
@@ -8,12 +8,13 @@ export default function BannerPrincipal({
   links,
   botoes,
   backgroundImage,
+  imagemCol,
 }) {
   return (
     <section id="section-1" className="pt-5">
-      <div className="container  c-txt-b py-5">
+      <div className="container c-txt-b py-5">
         <div
-          className="row row-banner w-100 p-4 my-"
+          className="row-banner row p-0 pt-5"
           style={{
             backgroundImage: `url(${backgroundImage})`,
             backgroundSize: "cover",
@@ -21,8 +22,9 @@ export default function BannerPrincipal({
             borderRadius: "24px",
           }}
         >
-          <div className="col-md-6 py-4 col-12 d-flex justify-content-between flex-column">
-            <div className="start-0 py-3 z-3">
+          {/* Coluna da Esquerda */}
+          <div className="col-12 col-md-6 d-flex flex-column justify-content-between p-4">
+            <div className="py-3 z-3">
               <Image
                 src={logo}
                 alt="Logo"
@@ -37,28 +39,28 @@ export default function BannerPrincipal({
             <div>
               <h3>{titulo}</h3>
               <div className="pt-3">
-                <div>
-                  <span>{descricao}</span>
-                </div>
-                <div className="d-flex pt-4 pb-2 gap-3 flex-wrap"></div>
+                <span>{descricao}</span>
               </div>
             </div>
 
-            <div className="d-flex gap-3 pt-3 flex-wrap">
+            {/* Botões */}
+            <div className="row pt-3 g-3">
               {botoes.map((btn, index) => (
-                <a
-                  key={index}
-                  href={btn.href}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="btn btn-outline-primary btn-banner rounded-pill px-4 py-2"
-                >
-                  {btn.label}
-                </a>
+                <div key={index} className="col-12 col-md-auto">
+                  <a
+                    href={btn.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="btn btn-outline-primary btn-banner rounded-pill px-4 py-2 w-100"
+                  >
+                    {btn.label}
+                  </a>
+                </div>
               ))}
             </div>
           </div>
 
+          {/* Coluna da Direita */}
           <div
             className="col-12 col-md-6"
             style={{
@@ -66,7 +68,17 @@ export default function BannerPrincipal({
               height: "100%",
               overflow: "hidden",
             }}
-          ></div>
+          >
+            <Image
+              src={imagemCol}
+              alt="Imagem da Direita"
+              width={500}
+              height={500}
+              sizes="100%"
+              className="img-fluid"
+              style={{ maxWidth: "100%", height: "auto" }}
+            />
+          </div>
         </div>
       </div>
     </section>
