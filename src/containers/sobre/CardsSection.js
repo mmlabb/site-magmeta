49d88ec18158671c82bf8951d1eee1b5c26ca453
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2025-07-09 10:34:33 -0300

    fix: Ajuste de background card quem somos

--- src/containers/sobre/CardsSection.js
+++ src/containers/sobre/CardsSection.js
@@ -8,75 +8,49 @@ import dynamics from "@/assets/lottie/dynamics.json";
 export default function CardsSection() {
   return (
     <>
-      <section>
+      <section className="py-5">
         <div className="container" id="boxs-sobre">
           <div className="row gx-2 gy-2 linha-box-sobre">
             <div className="col flex-1">
               <div
-                id="box-sobre-3"
                 data-aos="fade-up"
                 data-aos-delay="400"
-                className="overflow-hidden"
+                className="overflow-hidden box-sobre-q"
               >
                 <span className="p-5 text-center text-white">
-                  <h3>Customização</h3>
+                  <h3 className="txt-s">Customização</h3>
                   Nossa abordagem personalizada garante que cada solução seja
                   ajustada às necessidades específicas, criando uma experiência
                   única e satisfatória
                 </span>
-                <div className="">
-                  <Lottie
-                    animationData={dynamics}
-                    className="flex justify-center items-center"
-                    loop={true}
-                    speed={0.25}
-                  />
-                </div>
               </div>
             </div>
 
             <div className="col flex-1">
               <div
-                id="box-sobre-2"
                 data-aos="fade-up"
                 data-aos-delay="300"
-                className="overflow-hidden"
+                className="overflow-hidden box-sobre-q"
               >
                 <span className="p-5 text-center text-white">
-                  <h3>Soluções integradas</h3>
+                  <h3 className="txt-s">Soluções integradas</h3>
                   Através de cada solução conseguimos formar um campo magnético
                   capaz de unir oportunidades e pessoas de sucesso.
                 </span>
-                <div className="">
-                  <Lottie
-                    animationData={system}
-                    className="flex justify-center items-center"
-                    loop={true}
-                    speed={0.4}
-                  />
-                </div>
               </div>
             </div>
             <div className="col flex-1">
               <div
-                id="box-sobre-1"
                 data-aos="fade-up"
                 data-aos-delay="200"
-                className="overflow-hidden"
+                className="overflow-hidden box-sobre-q"
               >
                 <span className="p-5 text-center text-white">
-                  <h3>Inovação contínua</h3>
+                  <h3 className="txt-s">Inovação contínua</h3>
                   Nossos produtos contam com as melhores tecnologias do mercado.
                   Com pessoas apaixonadas por inovar, estamos sempre atualizados
                   das melhores soluções para escalar.
                 </span>
-                <div className="">
-                  <Lottie
-                    animationData={mission}
-                    className="flex justify-center items-center"
-                    loop={true}
-                  />
-                </div>
               </div>
             </div>
           </div>
