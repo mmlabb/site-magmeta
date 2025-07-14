d27aca4d7f6f8cbe00e25d1ff84d0b23d1f06752
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2025-07-14 13:57:54 -0300

    fix: Delete row texto-construimos-juntos

--- src/containers/Customers.js
+++ src/containers/Customers.js
@@ -1,15 +1,9 @@
 import Image from "next/image";
 
-
 export default function Clientes() {
   return (
     <section id="quarta-sessao" className="pt-5 mb-5 pt-5 mt-5">
       <div className="container text-center cont-logos pb-5 mb-5">
-        <div className="row texto-construimos-juntos ">
-          <div className="col pb-5">
-            <h2>Já construímos juntos com:</h2>
-          </div>
-        </div>
         <div className="row linha-logos  pb-5 align-items-center justify-content-center">
           <div className="col-md-4 col-6">
             <Image
