3628abdfb2b95fbd0e763a752f8c21bbb435f0a9
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2025-07-16 03:39:02 -0300

    feat: inserindo logo marq customrs

--- src/containers/Customers.js
+++ src/containers/Customers.js
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Clientes() {
   return (
-    <section id="quarta-sessao" className="pt-5 mb-5 pt-5 mt-5">
-      <div className="container text-center cont-logos pb-5 mb-5">
+    <section id="quarta-sessao" className="py-5 mt-5">
+      <div className="container text-center cont-logos">
         <div className="row linha-logos  pb-5 align-items-center justify-content-center">
-          <div className="col-md-4 col-6">
+          <div className="col-md-3 col-6">
             <Image
               src="/assets/img/logo-cadcam-white.svg"
               alt="logo-usados-cad"
@@ -13,10 +13,10 @@ export default function Clientes() {
               width={0}
               height={0}
               sizes="100%"
-              style={{ width: "auto", height: "auto" }} 
+              style={{ width: "auto", height: "auto" }}
             />
           </div>
-          <div className="col-md-4 col-6">
+          <div className="col-md-3 col-6">
             <Image
               src="/assets/img/logo-mclove-white.svg"
               alt="logo-mclove"
@@ -24,13 +24,24 @@ export default function Clientes() {
               width={0}
               height={0}
               sizes="100%"
-              style={{ width: "auto", height: "auto" }} 
+              style={{ width: "auto", height: "auto" }}
             />
           </div>
-          <div className="col-md-4 col-6">
+          <div className="col-md-3 col-6">
             <Image
               src="/assets/img/logo-klubi-white.svg"
               alt="logo-klubi"
+              className="logo-marca logo"
+              width={0}
+              height={0}
+              sizes="100%"
+              style={{ width: "auto", height: "auto" }}
+            />
+          </div>
+          <div className="col-md-3 col-6">
+            <Image
+              src="/assets/img/logo-marq-black.svg"
+              alt="logo-marq"
               className="logo-marca logo"
               width={0}
               height={0}
