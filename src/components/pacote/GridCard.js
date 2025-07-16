43341ed4248f89a68f9ef75b34d12951d3d51e73
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2025-07-16 03:48:09 -0300

    fix: Ajuste de props para home componente GridCard

--- src/components/pacote/GridCard.js
+++ src/components/pacote/GridCard.js
@@ -1,11 +1,17 @@
 "use client";
-import { BsCheckCircleFill } from "react-icons/bs";
+import { BsCheckCircleFill, BsCheckCircle } from "react-icons/bs";
 
-export default function GridCard({ titulo, passos, ativoBgColor, iconColor }) {
+export default function GridCard({
+  titulo,
+  passos,
+  ativoBgColor,
+  iconColor,
+  modoHome = false,
+}) {
   return (
-    <section id="section-2" className="py-5 my-5">
+    <section id="section-2" className="pt-5 my-5">
       <div className="container">
-        <div className="row text-center py-5 mb-4">
+        <div className="row text-center py-3 mb-4">
           <h4>{titulo}</h4>
         </div>
         <div className="row d-flex row-cols-1 justify-conten-start row-cols-md-3 g-4">
@@ -13,25 +19,36 @@ export default function GridCard({ titulo, passos, ativoBgColor, iconColor }) {
             const isFirstRow = i < 3;
             const isMiddle = i === 1 || i === 4;
             const isAtivo =
-              (isFirstRow && !isMiddle) || (!isFirstRow && isMiddle);
+              modoHome ||
+              (isFirstRow && !isMiddle) ||
+              (!isFirstRow && isMiddle);
 
             return (
               <div className="col" key={i}>
                 <div
                   className={`card-grid-bloco ${isAtivo ? "ativo" : ""}`}
-                  style={
-                    isAtivo ? { backgroundColor: ativoBgColor } : undefined
-                  }
+                  style={{
+                    backgroundColor: isAtivo ? ativoBgColor : undefined,
+                    color: modoHome ? "#000" : undefined,
+                  }}
                 >
-                  <BsCheckCircleFill
-                    size={40}
-                    color={iconColor}
-                    style={{
-                      backgroundColor: "#ffffff",
-                      borderRadius: "50%",
-                    }}
-                    className="mb-3"
-                  />
+                  {modoHome ? (
+                    <BsCheckCircle
+                      size={40}
+                      color={iconColor}
+                      className="mb-3"
+                    />
+                  ) : (
+                    <BsCheckCircleFill
+                      size={40}
+                      color={iconColor}
+                      style={{
+                        backgroundColor: "#ffffff",
+                        borderRadius: "50%",
+                      }}
+                      className="mb-3"
+                    />
+                  )}
                   <h4 className="mb-2">{item.title}</h4>
                   <p className="mb-0">{item.text}</p>
                 </div>
