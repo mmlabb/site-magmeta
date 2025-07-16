014dc4dc1f5e500511a6ee391403e93bb2a11c52
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2025-07-16 03:47:29 -0300

    fix: Ajuste de props cta componente

--- src/components/pacote/CtaAcao.js
+++ src/components/pacote/CtaAcao.js
@@ -5,6 +5,8 @@ export default function CtaAcao({
   descricao,
   botaoPrimario,
   botaoSecundario,
+  botaoCustomPrimario,
+  botaoCustomSecundario,
 }) {
   return (
     <section className="py-3">
@@ -12,28 +14,45 @@ export default function CtaAcao({
         <div className="row align-items-center">
           {/* Coluna da esquerda */}
           <div className="col-md-6">
-            <div className="py-4 box-ct">
+            <div className=" gap-2 d-flex  flex-column box-ct">
               <h3>{titulo}</h3>
               <p className="text-break">{descricao}</p>
-              {botaoPrimario && (
-                <button className="btn btn-banner btn-outline-primary rounded-pill px-4 mt-3">
-                  {botaoPrimario.label}
-                </button>
-              )}
+
+              <div>
+                {botaoCustomPrimario ? (
+                  botaoCustomPrimario
+                ) : botaoPrimario ? (
+                  <button
+                    className={`btn btn-banner btn-outline-primary rounded-pill px-4 mt-3 ${
+                      botaoPrimario.className || ""
+                    }`}
+                  >
+                    {botaoPrimario.label}
+                  </button>
+                ) : null}
+              </div>
             </div>
           </div>
 
           {/* Coluna da direita */}
-          <div className="col-md-5">
-            <div className="pt-4 box-ct text-md-end">
-              {botaoSecundario && (
+          <div className="col-md-6">
+            <div className="box-ct d-flex flex-column gap-5 px-4 text-md-end">
+              {botaoCustomSecundario ? (
+                botaoCustomSecundario
+              ) : botaoSecundario ? (
                 <>
                   <h3>{botaoSecundario.titulo}</h3>
-                  <button className="btn btn-banner my-3 btn-outline-primary rounded-pill px-4 mt-3">
-                    {botaoSecundario.label}
-                  </button>
+                  <div>
+                    <button
+                      className={`btn btn-banner my-3 btn-outline-primary rounded-pill px-4 mt-3 ${
+                        botaoSecundario.className || ""
+                      }`}
+                    >
+                      {botaoSecundario.label}
+                    </button>
+                  </div>
                 </>
-              )}
+              ) : null}
             </div>
           </div>
         </div>
