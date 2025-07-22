6c7c76280a932422d2bee2038aed02f585e4f015
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2025-07-22 08:58:32 -0300

    chore: Ajuste de target redes sociais footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -50,7 +50,11 @@ export default function Footer() {
         <div className="row pt-4">
           <div className="col d-flex justify-content-center align-items-center">
             <div className="mx-2 my-2 d-flex">
-              <a href="https://www.facebook.com/profile.php?id=61560382723312">
+              <a
+                href="https://www.facebook.com/profile.php?id=61560382723312"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <Image
                   src="/assets/img/icon-face-white.svg"
                   alt="logo-facebook"
@@ -63,7 +67,11 @@ export default function Footer() {
               </a>
             </div>
             <div className="mx-2 my-2">
-              <a href="https://www.instagram.com/magmetasolucoes/">
+              <a
+                href="https://www.instagram.com/magmetasolucoes/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <Image
                   src="/assets/img/logo-instagram-white.svg"
                   alt="logo-instagram"
