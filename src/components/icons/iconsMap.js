c40ed60bdde55aae7440ba1aec0f3473570fe17d
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2025-07-23 17:01:17 -0300

    componente icon awesome

---
+++ src/components/icons/iconsMap.js
@@ -0,0 +1,26 @@
+// src/icons/iconsMap.js
+import {
+  faStethoscope,
+  faGavel,
+  faHome,
+  faCut,
+  faUtensils,
+  faDraftingCompass,
+  faCarSide,
+  faDumbbell,
+  faPaw,
+  faCameraRetro,
+} from "@fortawesome/free-solid-svg-icons";
+
+export const icones = {
+  "fa-stethoscope": faStethoscope,
+  "fa-gavel": faGavel,
+  "fa-home": faHome,
+  "fa-cut": faCut,
+  "fa-utensils": faUtensils,
+  "fa-drafting-compass": faDraftingCompass,
+  "fa-car-side": faCarSide,
+  "fa-dumbbell": faDumbbell,
+  "fa-paw": faPaw,
+  "fa-camera-retro": faCameraRetro,
+};
