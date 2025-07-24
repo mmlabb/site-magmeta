ea19c0425dea3f5e9fc854d95ab1bd87c26096a2
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2025-07-24 12:47:16 -0300

    feat: novas props para cor dos buttons planos

--- src/components/pacote/PlanosSection.js
+++ src/components/pacote/PlanosSection.js
@@ -1,7 +1,21 @@
 "use client";
 import { FaCheckCircle } from "react-icons/fa";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMedal, faTrophy, faGem } from "@fortawesome/free-solid-svg-icons";
 
-export default function PlanosSection({ planos, destaqueBgColor, destaqueTextColor, checkIconColor, checkIconColorNaoDestaque }) {
+export default function PlanosSection({
+  planos,
+  destaqueBgColor,
+  destaqueTextColor,
+  checkIconColor,
+  checkIconColorNaoDestaque,
+  destaqueBtnColor,
+  iconeCor,
+  corBotaoNaoDestaque,
+  corTextoBotaoNaoDestaque
+}) {
+  const icons = [faMedal, faTrophy, faGem]; // essencial, impulso, premium
+
   return (
     <section id="section-3" className="py-5 planos">
       <div className="container py-5">
@@ -11,19 +25,24 @@ export default function PlanosSection({ planos, destaqueBgColor, destaqueTextCol
         <div className="row r-gap">
           {planos.map((plano, index) => {
             const isDestaque = index === 1;
+            const Icone = icons[index];
 
             return (
               <div className="col-12 col-md-4 mb-4" key={index}>
-                <div className="faixa-titulo">
-                  <h5 className="titulo-plano text-white m-0">{plano.nome}</h5>
+                <div className="faixa-titulo d-flex align-items-center justify-content-center gap-2">
+                  <FontAwesomeIcon icon={Icone} color={iconeCor} size="lg" />
+                  <h5 id="txt-plan" className="titulo-plano m-0">
+                    {plano.nome}
+                  </h5>
                 </div>
                 <div
-                  className={`plano-box ${
-                    isDestaque ? "plano-destaque" : ""
-                  }`}
+                  className={`plano-box ${isDestaque ? "plano-destaque" : ""}`}
                   style={
                     isDestaque
-                      ? { background: destaqueBgColor, color: destaqueTextColor, }
+                      ? {
+                          background: destaqueBgColor,
+                          color: destaqueTextColor,
+                        }
                       : undefined
                   }
                 >
@@ -31,7 +50,9 @@ export default function PlanosSection({ planos, destaqueBgColor, destaqueTextCol
                     {plano.etiqueta && (
                       <div className="etiqueta">{plano.etiqueta}</div>
                     )}
-                    <div className="preco pt-4">{plano.preco}</div>
+                    <div id="text-plano" className="preco pt-4">
+                      {plano.preco}
+                    </div>
                     <div className="descricao">{plano.descricao}</div>
                     <div className="d-flex flex-column justify-content-between">
                       <ul className="beneficios">
@@ -42,7 +63,11 @@ export default function PlanosSection({ planos, destaqueBgColor, destaqueTextCol
                           >
                             <FaCheckCircle
                               className="me-2 mt-1"
-                              color={isDestaque ? checkIconColor : checkIconColorNaoDestaque}
+                              color={
+                                isDestaque
+                                  ? checkIconColor
+                                  : checkIconColorNaoDestaque
+                              }
                               style={{ minWidth: "16px", minHeight: "16px" }}
                             />
                             <span>{item}</span>
@@ -52,7 +77,22 @@ export default function PlanosSection({ planos, destaqueBgColor, destaqueTextCol
                     </div>
                   </div>
                   <div className="text-center mt-3">
-                    <button className="btn btn-outline-primary btn-white btn-succe">
+                    <button
+                      className="btn btn-outline-primary btn-white btn-succe"
+                      style={
+                        isDestaque
+                          ? {
+                              backgroundColor: destaqueBtnColor, 
+                              color: "#fff", 
+                              border: "none",
+                            }
+                          : {
+                              backgroundColor: corBotaoNaoDestaque, 
+                              color: corTextoBotaoNaoDestaque, 
+                              border: "1px solid #000",
+                            }
+                      }
+                    >
                       Experimente grátis
                     </button>
                   </div>
