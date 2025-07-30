4431da85edb1b9bc1cb68de53ed032bdb60a00a5
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2025-07-30 18:12:37 -0300

    chore: Ajuste de text-center componente Titulo

--- src/components/TituloeSub.js
+++ src/components/TituloeSub.js
@@ -2,7 +2,7 @@
 
 export default function TituloSecao({ titulo, subtitulo }) {
   return (
-    <div className="d-flex justify-content-center text-center  flex-column gap-2">
+    <div className="d-flex justify-content-center flex-column gap-2">
       <h3>{titulo}</h3>
       <h5 className="txt-escolha">{subtitulo}</h5>
     </div>
