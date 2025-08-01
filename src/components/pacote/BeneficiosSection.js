1b3d8857b10e64be7c0d1af4eca9bd933500c2ce
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2025-08-01 16:37:32 -0300

    fix: Ajuste de flex componente beneficios

--- src/components/pacote/BeneficiosSection.js
+++ src/components/pacote/BeneficiosSection.js
@@ -14,18 +14,12 @@ export default function BeneficiosSection({
   compact = false,
 }) {
   return (
-    <div className={`container ${compact ? "" : "py-5"}`}>
+    <div className={`container ${compact}`}>
       <div
-        className={`row d-flex justify-content-center align-items-center ${
-          compact ? "" : "py-5"
+        className={`row d-flex justify-content-between align-items-center ${
+          compact ? "" : ""
         }`}
       >
-        {!compact && (
-          <div className="d-flex justify-content-center py-5">
-            <h3>{titulo}</h3>
-          </div>
-        )}
-
         {showLottie && (
           <div className="col-md-5 d-flex justify-content-center mb-4 mb-md-0">
             <Lottie
@@ -43,19 +37,24 @@ export default function BeneficiosSection({
         )}
 
         <div className={showLottie ? "col-md-6" : "col-12"}>
-          {!compact && <p className="pb-3">{descricao}</p>}
-          <ul className="list-unstyled mb-0 mt-0">
-            {beneficios.map((item, index) => (
-              <li key={index} className="d-flex align-items-start mb-2">
-                <FaCheck
-                  className="me-2 mt-1"
-                  color={checkColor}
-                  size={16}
-                />
-                <span>{item}</span>
-              </li>
-            ))}
-          </ul>
+          {!compact && (
+            <div className="d-flex gap-3 flex-column">
+              <h3>{titulo}</h3>
+              {!compact && <p>{descricao}</p>}
+              <ul className="list-unstyled mb-0 mt-0">
+                {beneficios.map((item, index) => (
+                  <li key={index} className="d-flex align-items-start mb-2">
+                    <FaCheck
+                      className="me-4 mt-1"
+                      color={checkColor}
+                      size={16}
+                    />
+                    <span>{item}</span>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
         </div>
       </div>
     </div>
