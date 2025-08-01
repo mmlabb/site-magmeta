f8e389d67f2dc5ad89ea4c49345a17d2913fdac4
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2025-08-01 20:34:35 -0300

    feat: prop para gradiente btn colorido

--- src/components/botoes/BtnColor.js
+++ src/components/botoes/BtnColor.js
@@ -1,4 +1,9 @@
-const BtnColorido = () => {
+"use client";
+
+const BtnColorido = ({ gradient }) => {
+  const defaultGradient =
+    "linear-gradient(to right, #9c5dd3 0%, #6221a2 33%, #03d1e8 100%)";
+
   return (
     <div className="btn-wrapper">
       <a
@@ -6,6 +11,9 @@ const BtnColorido = () => {
           "Olá, vim pelo site e gostaria de saber mais!"
         )}`}
         className="btn-colorido"
+        style={{
+          background: gradient || defaultGradient,
+        }}
         target="_blank"
         rel="noopener noreferrer"
         data-content="Quero começar agora!"
