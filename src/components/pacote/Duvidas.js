453e2a1637938838cd62a447c00fdcae031f4aa2
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2025-08-01 16:38:18 -0300

    fix: remove section do componente Duvidas e pequenos ajustes de flex

--- src/components/pacote/Duvidas.js
+++ src/components/pacote/Duvidas.js
@@ -4,8 +4,8 @@ import { FaWhatsapp } from "react-icons/fa";
 
 export default function Duvidas({ backgroundImage, textos, botaoLabel }) {
   return (
-    <section
-      className="mt-5"
+    <div
+      className="container"
       style={{
         backgroundImage: `url(${backgroundImage})`,
         backgroundSize: "cover",
@@ -13,42 +13,40 @@ export default function Duvidas({ backgroundImage, textos, botaoLabel }) {
         borderRadius: "24px",
       }}
     >
-      <div className="container">
-        <div className="row align-items-center text-white py-5">
-          {/* Coluna de textos */}
-          <div className="col-md-7 mb-4 mb-md-0 px-5">
-            <div className="d-flex flex-column gap-2">
-              <div className="d-flex justify-content-start ">
-                <FaWhatsapp size={54} color="#25D366" className="mb-2" />
-              </div>
-              {textos.map((texto, index) => (
-                <h4
-                  id="txt-cta-wpp"
-                  key={index}
-                  className="mb-3 duvidas-section text-cta fw-bold"
-                  style={{ color: "#fff" }}
-                >
-                  {texto}
-                </h4>
-              ))}
-              <p id="txt-envie" style={{ color: "#fff" }}>
-                Envie uma mensagem e converse com nosso time agora mesmo!
-              </p>
+      <div className="row align-items-center text-white p-5">
+        {/* Coluna de textos */}
+        <div className="col-md-7  mb-md-0 px-5">
+          <div className="d-flex flex-column gap-2">
+            <div className="d-flex justify-content-start ">
+              <FaWhatsapp size={54} color="#25D366" className="mb-2" />
             </div>
-          </div>
-
-          {/* Coluna do botão */}
-          <div id="btn-wp" className="col-md-5 text-md-end text-center  p-5">
-            <a
-              href="#"
-              className="btn btn-success btn-succe d-inline-flex align-items-center gap-2 px-4 py-2 rounded-pill"
-            >
-              <BsWhatsapp size={20} />
-              {botaoLabel}
-            </a>
+            {textos.map((texto, index) => (
+              <h4
+                id="txt-cta-wpp"
+                key={index}
+                className="mb-3 duvidas-section text-cta fw-bold"
+                style={{ color: "#fff" }}
+              >
+                {texto}
+              </h4>
+            ))}
+            <p id="txt-envie" style={{ color: "#fff" }}>
+              Envie uma mensagem e converse com nosso time agora mesmo!
+            </p>
           </div>
         </div>
+
+        {/* Coluna do botão */}
+        <div id="btn-wp" className="col-md-5  text-center ">
+          <a
+            href="#"
+            className="btn btn-success btn-succe d-inline-flex align-items-center gap-2 px-4 py-2 rounded-pill"
+          >
+            <BsWhatsapp size={20} />
+            {botaoLabel}
+          </a>
+        </div>
       </div>
-    </section>
+    </div>
   );
 }
