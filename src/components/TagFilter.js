1785f91e8d539a2bd72d8c2e8ed8fe289e0c568b
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2025-08-01 17:07:59 -0300

    chore: removi paddings

--- src/components/TagFilter.js
+++ src/components/TagFilter.js
@@ -4,8 +4,7 @@ import "../app/blog/blog.css";
 
 const TagFilter = ({ tags, selectedTag }) => {
   return (
-    <div className="box-tags d-flex gap-3 justify-content-center flex-wrap mb-4">
-      
+    <div className="box-tags container d-flex gap-3 justify-content-center flex-wrap">
       {/* Botões de tags */}
       {tags.map((tag) => (
         <Button
