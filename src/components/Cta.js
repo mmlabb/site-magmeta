c179232749df3d5410a357ed4cdd2ee88cc5f05d
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2025-08-01 17:07:32 -0300

    fix: Add gap e delete padding

--- src/components/Cta.js
+++ src/components/Cta.js
@@ -3,9 +3,9 @@ import { Arrow90degRight, ArrowRightShort, ArrowRightSquare } from "react-bootst
 import { ArrowDown, ArrowDownLeft, ArrowDownRight, ArrowLeft, ArrowRight, ArrowUpRight } from "react-feather";
 const CTA = () => {
   return (
-    <div className=" container box-cta py-5 text-center">
+    <div className=" container box-cta p-5 text-center d-flex flex-colunm gap-5">
       <h3>Transforme ideias em soluções tecnológicas que geram resultados. Fale com a Magmeta Soluções e leve seu negócio para o próximo nível.</h3>
-      <div className="d-flex mt-4 justify-content-center">
+      <div className="d-flex justify-content-center">
         <a
           href="https://onboarding.magmeta.com.br/onboarding/lead"
           className="btn-mobile-orcam texto-botao botao-nossa-ex btn-or botao-roxo"
