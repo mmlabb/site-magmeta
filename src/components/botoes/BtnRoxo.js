55108599d1da065afd7518ae290cefb036a82a57
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2025-08-13 17:41:44 -0300

    fix: remove html <a> do btnroxo

--- src/components/botoes/BtnRoxo.js
+++ src/components/botoes/BtnRoxo.js
@@ -1,10 +1,5 @@
-"use client";
-
-import { CalendarCheck } from "react-bootstrap-icons";
-
 export default function BtnRoxo({
   children = "Agendar sessão estratégica",
-  icon = <CalendarCheck size={16} className="icon" />,
   className = "",
   style = {},
   variant = "default",
@@ -13,20 +8,18 @@ export default function BtnRoxo({
     default: "linear-gradient(to right, #5B19A9 100%,  #9C5DD3 100%)",
     site: "linear-gradient(to right, #03d1e8 0%, #1197c3 50%, #005b81 100%)",
     chat: "linear-gradient(to right, #00c37e 0%, #1ac881 50%, #42d391 100%)",
+    app: "linear-gradient(to right, #C900A4 0%, #9A007F 100%)",
   };
 
   return (
-    <a
-      href="https://wa.me/5585988756563?text=Olá%20equipe%20Magmeta,%20quero%20agendar%20uma%20reunião."
-      target="_blank"
-      rel="noopener noreferrer"
+    <button
       className={`btn-purple btn-sm ${className}`}
       style={{
         background: gradients[variant] || gradients.default,
         ...style,
       }}
     >
-      <span>{children}</span>
-    </a>
+      {children}
+    </button>
   );
 }
