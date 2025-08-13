4181af44193a0f6d1ece908b3eb3e29bf098feac
Vanz-silva <178945116+Vanz-silva@users.noreply.github.com>
2025-08-13 18:36:38 -0300

    fix: ajuste de tempo AOS

--- src/components/AOSInit.js
+++ src/components/AOSInit.js
@@ -6,7 +6,7 @@ import AOS from "aos";
 export default function AOSInit() {
   useEffect(() => {
     AOS.init({
-      duration: 2000, // duração da animação
+      duration: 1500, // duração da animação
       once: false, // false = anima toda vez que entra na viewport
       offset: 50, // distância antes de disparar
     });
